5b50aeb3ff95677ba94fd46adadabf850ab37425
JonnyDavison <>
2023-05-09 12:09:21 +0000

    Update sendEmail.js fucntion

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -3,7 +3,7 @@ function sendMail(contactForm){
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
-    });
+    })
     .then(
         function(response) {
             console.log("SUCCESS", response);
